d0ff682930d5880055e5c0c3c612e2d81d7e76bc
Türei Dénes <turei.denes@gmail.com>
2017-09-05 11:23:57 +0200

    lipid module created

---
+++ emese/lipid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `emese` python module
+#
+#  Copyright (c) 2015-2017 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+from future.utils import iteritems
+
+class Lipid(object):
+    
+    def __init__(self, name = 'Lipid', num_fa = 2, type = 'GLP'):
+        
+        
