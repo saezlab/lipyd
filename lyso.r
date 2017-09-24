32c79d840212c9e3ce2f85d70ab6c28c7ac05a6d
Türei Dénes <turei.denes@gmail.com>
2017-09-24 14:14:15 +0200

    lyso-ether: added preference barplots and dotplots

--- lyso.r
+++ lyso.r
@@ -5,6 +5,9 @@
 
 require(ggplot2)
 require(dplyr)
+require(lazyeval)
+require(stringr)
+require(ggrepel)
 
 #
 # constants
@@ -14,7 +17,7 @@ infile_c12 <- 'combined_network_df.csv'
 infile_c1  <- 'combined_network_classI_df.csv'
 infile_a   <- 'antonella_final.csv'
 infile_e   <- 'enric_processed.csv'
-gpl <- c('PE', 'PC', 'PG', 'PA', 'PI', 'PIP', 'PS', 'DAG', 'FA')
+gpl <- c('PE', 'PC', 'PG', 'PA', 'PI', 'PS', 'DAG', 'FA')
 mss <- c('MSA', 'MSE')
 
 #
@@ -51,6 +54,7 @@ preprocess <- function(infile, by_hg = TRUE){
     # order by domain and protein name
     data <- data %>%
         filter(datasource %in% mss) %>%
+        mutate(gsub('MAG', 'LysoDAG', uhgroup)) %>%
         arrange(domain, protein) %>%
         mutate(
             protein = factor(protein, unique(protein)),
@@ -119,8 +123,12 @@ preprocess2 <- function(by_hg = TRUE, only_class1 = TRUE){
     ae <- rbind(a, e)
     
     ae <- ae %>%
+        mutate(gsub('MAG', 'LysoDAG', uhgroup)) %>%
         group_by(protein) %>%
-        mutate(screens = paste0(unique(sort(screen)), collapse = '')) %>%
+        mutate(
+            screens = paste0(unique(sort(screen)), collapse = ''),
+            protein0 = protein
+        ) %>%
         ungroup() %>%
         mutate(
             protein = ifelse(
@@ -210,7 +218,7 @@ preprocess2 <- function(by_hg = TRUE, only_class1 = TRUE){
     
 }
 
-do_plot <- function(df, pdfname, title, by_hg = TRUE, intensities = FALSE){
+intensity_bubble <- function(df, pdfname, title, by_hg = TRUE, intensities = FALSE){
     
     width <- ifelse(by_hg, 12, 6)
     
@@ -312,19 +320,131 @@ do_plot <- function(df, pdfname, title, by_hg = TRUE, intensities = FALSE){
     ggsave(pdfname, device = cairo_pdf, width = width, height = 8)
 }
 
+align_by_hg0 <- function(d){
+    
+    d0 <- d %>%
+        select(
+            protein0, datasource, lysoether, lyso, lyso2, ether,
+            screen, screens, cls, ionm, uhgroup, hg0, irel
+        ) %>%
+        droplevels() %>%
+        group_by(protein0, datasource, cls, uhgroup) %>%
+        mutate(clsh = first(sort(cls))) %>%
+        summarise_all(first) %>%
+        ungroup()
+    
+    lyso  <- d0 %>% filter(lyso2 & !ether)
+    ether <- d0 %>% filter(ether & !lyso2)
+    lyset <- d0 %>% filter(ether & lyso2)
+    ester <- d0 %>% filter(!lyso2 & !ether)
+    
+    joincols <- c('protein0', 'datasource', 'hg0')
+    
+    full  <- ester %>%
+        full_join(
+            ether,
+            by = joincols,
+            suffix = (c('.ester', '.ether'))
+        ) %>%
+        full_join(
+            lyso,
+            by = joincols,
+            suffix = (c('', '.lyso'))
+        ) %>%
+        full_join(
+            lyset,
+            by = joincols,
+            suffix = (c('.lyso', '.lyset'))
+        ) %>%
+        mutate(
+            irel.ester = ifelse(is.na(irel.ester), 0.0, irel.ester),
+            irel.ether = ifelse(is.na(irel.ether), 0.0, irel.ether),
+            irel.lyso  = ifelse(is.na(irel.lyso ), 0.0, irel.lyso ),
+            irel.lyset = ifelse(is.na(irel.lyset), 0.0, irel.lyset)
+        ) %>%
+        mutate(
+            ifc.ether  = ifelse(irel.ester == 0 & irel.ether == 0, 0.0, (irel.ether / (irel.ester + irel.ether) - 0.5) * 2.0),
+            ifc.lyso   = ifelse(irel.ester == 0 & irel.lyso  == 0, 0.0, (irel.lyso  / (irel.ester + irel.lyso ) - 0.5) * 2.0),
+            ifc.lyset  = ifelse(irel.ester == 0 & irel.lyset == 0, 0.0, (irel.lyset / (irel.ester + irel.lyset) - 0.5) * 2.0)
+        )
+    
+    return(full)
+    
+}
+
+pref_bar <- function(data, y){
+    
+    label <- strsplit(deparse(substitute(y)), '\\.')[[1]][2]
+    pdfname <- sprintf('pref-%s.pdf', label)
+    
+    p <- ggplot(data, aes_(x = quote(datasource), substitute(y), fill = substitute(y) < 0)) +
+        geom_col() +
+        facet_grid(protein0 ~ hg0) +
+        scale_fill_manual(
+            values = c(
+                'FALSE' = '#1B7D8D',
+                'TRUE'  = '#F9382D'
+            ),
+            labels = c(
+                'FALSE' = sprintf('Preferring %s', label),
+                'TRUE'  = 'Preferring 2x ester'
+            ),
+            guide = guide_legend(title = 'Preference')
+        ) +
+        ggtitle(sprintf('Preference towards %s species', label)) +
+        xlab('Lipid') +
+        ylab('Protein') +
+        theme_bw() +
+        theme(
+            text = element_text(family = "DINPro"),
+            axis.text.x = element_text(angle = 90, vjust = 0.5, size = 10, hjust = 1),
+        )
+    
+    ggsave(pdfname, device = cairo_pdf, width = 6, height = 24)
+    
+}
+
+pref_point <- function(data, y, labsize = 2, labangle = 45){
+    
+    label <- strsplit(deparse(substitute(y)), '\\.')[[1]][2]
+    pdfname <- sprintf('pref-dot-%s.pdf', label)
+    
+    p <- ggplot(data,
+            aes_(
+                x = quote(irel.ester + 1e-9),
+                y = substitute(y + 1e-9),
+                color = quote(hg0),
+                label = quote(protein0)
+            )
+        ) +
+        scale_x_log10() +
+        scale_y_log10() +
+        scale_color_discrete(guide = guide_legend(title = 'Headgroup')) +
+        geom_text(family = 'DINPro', angle = labangle, size = labsize) +
+        ggtitle(sprintf('Preference towards %s species', label)) +
+        xlab('2x ester relative intensity (log)') +
+        ylab(sprintf('%s relative intensity (log)', str_to_title(label))) +
+        theme_bw() +
+        theme(
+            text = element_text(family = "DINPro")
+        )
+    
+    ggsave(pdfname, device = cairo_pdf, width = 8, height = 8)
+    
+}
 
 # data_1     <- preprocess(infile_c1,  by_hg = FALSE)
 # data_12    <- preprocess(infile_c12, by_hg = FALSE)
 # data_1_hg  <- preprocess(infile_c1,  by_hg = TRUE)
 # data_12_hg <- preprocess(infile_c12, by_hg = TRUE)
 # 
-# do_plot(data_1,  'gg_combined_cargo_classI_lyso_ether_1.pdf',
+# intensity_bubble(data_1,  'gg_combined_cargo_classI_lyso_ether_1.pdf',
 #         'Lyso and ether species: only class I', by_hg = FALSE)
-# do_plot(data_12, 'gg_combined_cargo_lyso_ether_1.pdf',
+# intensity_bubble(data_12, 'gg_combined_cargo_lyso_ether_1.pdf',
 #         'Lyso and ether species: with class II from in vivo', by_hg = FALSE)
-# do_plot(data_1_hg,  'gg_combined_cargo_classI_lyso_ether.pdf',
+# intensity_bubble(data_1_hg,  'gg_combined_cargo_classI_lyso_ether.pdf',
 #         'Lyso and ether species: only class I', by_hg = TRUE)
-# do_plot(data_12_hg, 'gg_combined_cargo_lyso_ether.pdf',
+# intensity_bubble(data_12_hg, 'gg_combined_cargo_lyso_ether.pdf',
 #         'Lyso and ether species: with class II from in vivo', by_hg = TRUE)
 
 data_1     <- preprocess2(by_hg = FALSE)
@@ -332,22 +452,29 @@ data_12    <- preprocess2(by_hg = FALSE, only_class1 = FALSE)
 data_1_hg  <- preprocess2(by_hg = TRUE)
 data_12_hg <- preprocess2(by_hg = TRUE, only_class1 = FALSE)
 
-do_plot(data_1,
+intensity_bubble(data_1,
         'gg_combined_cargo_classI_lyso_ether_int_1.pdf',
         'Lyso and ether species: only class I',
         by_hg = FALSE, intensities = TRUE)
 
-do_plot(data_12,
+intensity_bubble(data_12,
         'gg_combined_cargo_lyso_ether_int_1.pdf',
         'Lyso and ether species: with class II',
         by_hg = FALSE, intensities = TRUE)
 
-do_plot(data_1_hg,
+intensity_bubble(data_1_hg,
         'gg_combined_cargo_classI_lyso_ether_int.pdf',
         'Lyso and ether species: only class I',
         by_hg = TRUE, intensities = TRUE)
 
-do_plot(data_12_hg,
+intensity_bubble(data_12_hg,
         'gg_combined_cargo_lyso_ether_int.pdf',
         'Lyso and ether species: with class II',
         by_hg = TRUE, intensities = TRUE)
+
+full <- align_by_hg0(data_12_hg)
+pref_bar(full, irel.ether)
+pref_bar(full, irel.lyso)
+
+pref_point(full, irel.ether)
+pref_point(full, irel.lyso)
