852ba23d524109a877e86f61184b33fe332f0912
Türei Dénes <turei.denes@gmail.com>
2018-02-03 22:30:11 +0100

    mzml.py created

---
+++ src/emese/mzml.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `emese` python module
+#
+#  Copyright (c) 2015-2018 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
