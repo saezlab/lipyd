6dbe1bc10614296731f9424fcfba010661b4eb6c
Türei Dénes <turei.denes@gmail.com>
2018-04-05 12:26:59 +0200

    lookup.py created

---
+++ src/lipyd/lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `lipyd` python module
+#
+#  Copyright (c) 2015-2018 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+from future.utils import iteritems
+
