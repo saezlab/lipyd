b24d934e3a7bae02ba74ff1913af1718ba3fc0b5
Türei Dénes <turei.denes@gmail.com>
2018-09-10 10:41:06 +0200

    ltp module created for LTP project specific code

---
+++ ltp/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `ltp` python module
+#
+#  Copyright (c) 2018 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
