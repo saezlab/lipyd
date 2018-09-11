52cb74fb5bcc2114005909551859d43e84afb629
Türei Dénes <turei.denes@gmail.com>
2018-09-11 14:31:20 +0200

    absolute paths in collect_mgf.sh

--- misc/collect_mgf.sh
+++ misc/collect_mgf.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-find . -depth -not -path '*deleteme*' -path '*/Results/*mgf' -exec ln -s {} ./mgf_invivo/ \;
-find . -depth -not -path '*deleteme*' -path '*update*/Results/*mgf' -exec ln -s -f {} ./mgf_invivo/ \;
+BASEDIR='/home/denes/archive/ltp'
+
+find $BASEDIR -depth -not -path '*deleteme*' -path '*/Results/*mgf' -exec ln -s -f {} $BASEDIR/mgf_invivo/ \;
+find $BASEDIR -depth -not -path '*deleteme*' -path '*update*/Results/*mgf' -exec ln -s -f {} $BASEDIR/mgf_invivo/ \;
 
 # fix file name typos
-for f in `ls *ORP9STARD15*`; do mv $f `echo $f | sed 's/STARD15//g'`; done
+for f in `ls $BASEDIR/*ORP9STARD15*`; do mv $f `echo $f | sed 's/STARD15//g'`; done
