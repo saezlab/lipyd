22026a754a05208e309d0fa7db729222c67330b2
Türei Dénes <turei.denes@gmail.com>
2018-09-13 18:04:14 +0200

    demo script

---
+++ lipyd_demo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  Copyright (c) 2018 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+import imp
+import pprint
+import os
+import itertools
+import sys
+import collections
+
+
+from lipyd import mass
+from lipyd import mz
+from lipyd import formula
+from lipyd import lipid
+from lipyd import metabolite
+from lipyd import substituent
+from lipyd import moldb
+from lipyd import fragment
+from lipyd import fragdb
+from lipyd import name
+from lipyd import ms2
+from lipyd import lipproc
+from lipyd import settings
+from lipyd import mgf
+
+
