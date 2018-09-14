b5ab3e4639a4990c31019d7164ac3388a8bd9e95
Türei Dénes <turei.denes@gmail.com>
2018-09-15 01:03:17 +0200

    jupyter html notebook header remover

--- remove_notebook_header.sh
+++ remove_notebook_header.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -iE 's/<!--\|-->//g;s/\(<div id="header"\)/<!-- \1/;s/\(<div id="site" style=".*">\)/\1 -->/' lipyd_demo.html
+sed -iE 's/<!--\|-->//g;s/\(<div id="header"\)/<!-- \1/;s/\(<div id="site" style=".*">\)/\1 -->/;s/\(id="notebook_panel"\)/\1 style="overflow:auto!important;"/' lipyd_demo.html
