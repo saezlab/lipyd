a24af09bb2da5b4b07e055a3ccf9e3ec40d9257b
Türei Dénes <turei.denes@gmail.com>
2018-10-29 23:16:20 +0100

    sampleattrs.SampleSetAttrs

--- src/lipyd/sampleattrs.py
+++ src/lipyd/sampleattrs.py
@@ -15,6 +15,8 @@
 #  Website: http://www.ebi.ac.uk/~denes
 #
 
+from past.builtins import xrange, range
+
 import numpy as np
 
 import lipyd.common as common
@@ -73,6 +75,115 @@ class SampleAttrs(object):
 
 class SampleSetAttrs(object):
     
-    def __init__(self):
+    def __init__(
+            self,
+            sample_ids_method = None,
+            sample_attrs = None,
+            sample_id_processor = None,
+            length = None,
+        ):
         
+        # custom method to process sample IDs
+        self._sample_id_processor = sample_id_processor
         
+        if isinstance(sample_ids_method, (list, np.ndarray)):
+            
+            # sample IDs provided for each sample
+            sample_ids = [
+                self._get_sample_id(sample_id)
+                for sample_id in sample_ids
+            ]
+            
+            length = len(sample_ids)
+            
+        elif sample_attrs is not None:
+            
+            # if sample attributes provided
+            length = len(sample_attrs)
+        
+        # now it is either None or a method
+        sample_ids = [sample_ids_method] * length
+        
+        if length is None:
+            
+            raise RuntimeError(
+                'SampleSetAttrs: number of samples not provided.'
+            )
+        
+        self.attrs = np.array([
+            SampleAttrs(
+                sample_id_method = sample_ids[i],
+                attrs = sample_attrs[i] if sample_attrs else {},
+            )
+            for i in xrange(length)
+        ])
+        
+        self._set_sample_ids()
+    
+    def _get_sample_id(self, sample_id):
+        
+        if callable(self._sample_id_processor):
+            
+            return self._sample_id_processor(sample_id)
+        
+        return sample_id
+    
+    def _set_sample_ids(self):
+        # Note: this is called by Sample.__init__()
+        
+        self.sample_index_to_id = []
+        
+        for attr in self.attrs:
+            
+            self.sample_index_to_id.append(attr.sample_id)
+        
+        self._update_id_to_index()
+    
+    def _update_id_to_index(self):
+        
+        self.sample_id_to_index = dict(
+            reversed(i)
+            for i in enumerate(self.sample_index_to_id)
+        )
+    
+    def get_sample_id(self, i):
+        """
+        Returns the ID of a sample by its index.
+        """
+        
+        return self.sample_index_to_id[i]
+    
+    def get_sample_index(self, sample_id):
+        """
+        Returns the index of a sample by its ID.
+        """
+        
+        return self.sample_id_to_index[sample_id]
+    
+    def sort_by_index(self, idx):
+        
+        idx = np.array(idx)
+        
+        self.attrs = self.attrs[idx]
+        
+        self._set_sample_ids()
+    
+    def sort_by_sample_id(self, sample_ids, process = False):
+        """
+        Sets the order of the sample attributes according to the list of
+        sample IDs provided.
+        
+        :param list sample_ids:
+            A list of sample IDs, e.g. ``[('A', 1), ('A', 2), ...]``.
+        :param bool process:
+            Process ``sample_ids`` by the ``sample_id_processor`` method.
+        """
+        
+        idx = np.array([
+            self.sample_id_to_index[
+                sample_id if not process else self._get_sample_id(sample_id)
+            ]
+            for sample_id in sample_ids
+        ])
+        
+        self.sort_by_index(idx)
