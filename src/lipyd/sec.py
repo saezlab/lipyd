bd4bc809580732bd0bfa0e5608b4948d08f68396
Türei Dénes <turei.denes@gmail.com>
2018-10-31 15:47:20 +0100

    sec module for handling SEC data

---
+++ src/lipyd/sec.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `lipyd` python module
+#
+#  Copyright (c) 2014-2018 - EMBL
+#
+#  File author(s): Dénes Türei (denes@ebi.ac.uk)
+#
+#  Distributed under the GNU GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+class SECProfile(object):
+    
+    def __init__(self):
+        
+        
