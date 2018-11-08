cec1d822f66aff78afac7563adae6d6fa10af16f
Türei Dénes <turei.denes@gmail.com>
2018-11-08 04:33:27 +0100

    test_feature

---
+++ src/test/test_feature.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `lipyd` python module
+#
+#  Copyright (c) 2015-2018 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  Distributed under the GNU GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+import pytest
+
+import lipyd.sample as sample
+import lipyd.feature as feature
+import lipyd.sampleattrs as sampleattrs
+
+
+class TestFeature(self):
+    
+    def test_feature_analyzer(self):
+        
+        
