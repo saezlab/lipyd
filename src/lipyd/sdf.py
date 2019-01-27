5297222b4ca972fd1ad7b7d01cf4a9af39917f9c
Türei Dénes <turei.denes@gmail.com>
2019-01-27 07:09:46 +0100

    updated years in file headers

--- src/lipyd/sdf.py
+++ src/lipyd/sdf.py
@@ -4,7 +4,7 @@
 #
 #  This file is part of the `lipyd` python module
 #
-#  Copyright (c) 2014-2018 - EMBL
+#  Copyright (c) 2015-2019 - EMBL
 #
 #  File author(s):
 #  Dénes Türei (turei.denes@gmail.com)
