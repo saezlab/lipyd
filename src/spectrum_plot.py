eeb9c93cc628df8e611db9d76fa6dea384988f14
Igor Bulanov <bulanov.ig@phystech.edu>
2019-01-29 09:46:19 +0100

    some changes in plot.py (nit final)

--- src/spectrum_plot.py
+++ src/spectrum_plot.py
@@ -1,33 +1,35 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+# -- 2019/01/22 --
 import os
-import imp
+import importlib
 
 import lipyd.ms2 as ms2
 import lipyd.common as common
 import lipyd.plot as plot
 import lipyd.settings as settings
 
-imp.reload(settings)
-imp.reload(ms2)
-imp.reload(plot)
 
 
-mgfname = 'pos_examples.mgf'
-ionmode = 'pos'
-scan_id = 2397
-scan_args = {}
 
-mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
-scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
+if __name__ == "__main__":
+    importlib.reload(settings)
+    importlib.reload(ms2)
+    importlib.reload(plot)
 
-p = plot.SpectrumPlot(
-    mzs = scan.mzs,
-    intensities = scan.intensities,
-    annotations = scan.annot,
-    scan_id = scan.scan_id,
-    ionmode = ionmode,
-    sample_name = 'BPI_invivo',
-    font_family = 'DINPro',
-)
+    mgfname = 'pos_examples.mgf'
+    ionmode = 'pos'
+    scan_id = 2397
+    scan_args = {}
+
+    mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
+    scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
+
+    p = plot.SpectrumPlot(
+        mzs = scan.mzs,
+        intensities = scan.intensities,
+        annotations = scan.annot,
+        scan_id = scan.scan_id,
+        ionmode = ionmode,
+        sample_name = 'BPI_invivo',
+        grid_cols = 1
+    )
+
