2a15796c82259d3c18ed868967280d159391a04d
Türei Dénes <turei.denes@gmail.com>
2019-01-30 10:57:27 +0100

    spectrum_plot removed __name__ == __main__

--- src/spectrum_plot.py
+++ src/spectrum_plot.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 # -- 2019/01/22 --
 import os
-import importlib
+import imp
 
 import lipyd.ms2 as ms2
 import lipyd.common as common
@@ -8,28 +10,24 @@ import lipyd.plot as plot
 import lipyd.settings as settings
 
 
+mgfname = 'pos_examples.mgf'
+ionmode = 'pos'
+scan_id = 2397
+scan_args = {}
+
+mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
+scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
 
 
-if __name__ == "__main__":
-    importlib.reload(settings)
-    importlib.reload(ms2)
-    importlib.reload(plot)
+imp.reload(plot)
 
-    mgfname = 'pos_examples.mgf'
-    ionmode = 'pos'
-    scan_id = 2397
-    scan_args = {}
-
-    mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
-    scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
-
-    p = plot.SpectrumPlot(
-        mzs = scan.mzs,
-        intensities = scan.intensities,
-        annotations = scan.annot,
-        scan_id = scan.scan_id,
-        ionmode = ionmode,
-        sample_name = 'BPI_invivo',
-        grid_cols = 1
-    )
+p = plot.SpectrumPlot(
+    mzs = scan.mzs,
+    intensities = scan.intensities,
+    annotations = scan.annot,
+    scan_id = scan.scan_id,
+    ionmode = ionmode,
+    sample_name = 'BPI_invivo',
+    grid_cols = 1
+)
 
