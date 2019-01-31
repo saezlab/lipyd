705c9ac8460d4afec64ffcf13cd100ade70db726
Igor Bulanov <bulanov.ig@phystech.edu>
2019-01-31 22:18:49 +0100

    delete if __name__==__main__ in spectrum_plot.py

--- src/spectrum_plot.py
+++ src/spectrum_plot.py
@@ -7,29 +7,28 @@ import lipyd.common as common
 import lipyd.plot as plot
 import lipyd.settings as settings
 
+    
+mgfname = 'pos_examples.mgf'
+ionmode = 'pos'
+scan_id = 2397
+scan_args = {}
+
+mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
+scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
 
 
 
-if __name__ == "__main__":
-    importlib.reload(settings)
-    importlib.reload(ms2)
-    importlib.reload(plot)
-
-    mgfname = 'pos_examples.mgf'
-    ionmode = 'pos'
-    scan_id = 2397
-    scan_args = {}
-
-    mgfpath = os.path.join(common.ROOT, 'data', 'ms2_examples', mgfname)
-    scan = ms2.Scan.from_mgf(mgfpath, scan_id, ionmode, **scan_args)
-
-    p = plot.SpectrumPlot(
-        mzs = scan.mzs,
-        intensities = scan.intensities,
-        annotations = scan.annot,
-        scan_id = scan.scan_id,
-        ionmode = ionmode,
-        sample_name = 'BPI_invivo',
-        grid_cols = 1
-    )
+importlib.reload(settings)
+importlib.reload(ms2)
+importlib.reload(plot)
+p = plot.SpectrumPlot(
+    mzs = scan.mzs,
+    intensities = scan.intensities,
+    annotations = scan.annot,
+    scan_id = scan.scan_id,
+    ionmode = ionmode,
+    sample_name = 'BPI_invivo',
+    grid_cols = 1,
+    format = "pdf"
+)
 
