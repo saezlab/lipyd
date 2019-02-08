7e476d6b4f21aa2e52616dfa2d0994c045c86137
Türei Dénes <turei.denes@gmail.com>
2019-02-08 15:33:29 +0100

    peaks reader example

--- misc/peaks_reader_example.py
+++ misc/peaks_reader_example.py
@@ -17,9 +17,25 @@ samples = reader.get_sampleset(
     }
 )
 
+idx = samples.mzs_by_sample[
+    :,samples.attrs.sample_id_to_index[('A', 10)]
+].argsort()
+
+samples.sort_all(by = idx)
+
 # m/z's for one sample:
 samples.mzs_by_sample[:,samples.attrs.sample_id_to_index[('A', 10)]]
 # intensities for one sample:
 samples.intensities[:,samples.attrs.sample_id_to_index[('A', 10)]]
 # RTs for one sample:
 samples.rts[:,samples.attrs.sample_id_to_index[('A', 10)]]
+
+# how to search:
+
+closest_index = lookup.find(
+    samples.mzs_by_sample[
+        :,samples.attrs.sample_id_to_index[('A', 10)]
+    ], # all masses in the sample
+    797.3319, # mass to search for
+    t = 10 # tolerance in ppm
+)
