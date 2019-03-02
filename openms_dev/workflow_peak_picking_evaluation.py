f773674465d2f5b1ae465c2718014bf607247f14
Türei Dénes <turei.denes@gmail.com>
2019-03-02 02:48:47 +0100

    peak picking evaluation workflow

--- openms_dev/workflow_peak_picking_evaluation.py
+++ openms_dev/workflow_peak_picking_evaluation.py
@@ -23,3 +23,37 @@ p = ppeval.PeakPickingEvaluation(
 )
 
 p.main()
+
+
+p = ppeval.PeakPickingEvaluation(
+    feature_xml_fname = os.path.join(
+        msproc_basedir,
+        'lipyd_ms_preproc',
+        '150310_Popeye_MLH_AC_STARD10_A10_pos',
+        '150310_Popeye_MLH_AC_STARD10_A10_pos__features.featureXML',
+    ),
+    peaks_fname = 'stard10_pos.csv',
+    examples_fname = 'STARD10_pos_examples.csv',
+    outfile = 'STARD10_ppeval__iterative__gaussian-20.tsv',
+    progenesis_fname = 'STARD10_invivo_pos_A10_isotopes.xml',
+)
+
+p.main()
+
+
+
+
+p = ppeval.PeakPickingEvaluation(
+    feature_xml_fname = os.path.join(
+        msproc_basedir,
+        'lipyd_ms_preproc',
+        '150310_Popeye_MLH_AC_STARD10_A10_pos',
+        '150310_Popeye_MLH_AC_STARD10_A10_pos__features.featureXML',
+    ),
+    peaks_fname = 'stard10_pos.csv',
+    examples_fname = 'STARD10_pos_examples.csv',
+    outfile = 'STARD10_ppeval__iterative__gaussian-10.tsv',
+    progenesis_fname = 'STARD10_invivo_pos_A10_isotopes.xml',
+)
+
+p.main()
