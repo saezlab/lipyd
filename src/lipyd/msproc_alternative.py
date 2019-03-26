b6734f5ffdc2285fce2a30cd9d0d7d5d16ff0632
IgorBulanov <bulanov@embl.de>
2019-03-26 18:43:00 +0100

    added mechnanism for multi uploading of files for peak picking and map alignment also

--- src/lipyd/msproc_alternative.py
+++ src/lipyd/msproc_alternative.py
@@ -61,6 +61,12 @@ class BaseEntity(object):
         self.entity.setParameters(param)
 
 
+class PPEntity(BaseEntity):
+    #oms.PeakPickingHiRes()
+    def __init__(self, **kwargs):
+        super(PPEntity, self).__init__(oms.PeakPickerHiRes(),
+                                                **kwargs)
+
 class MtdEntity(BaseEntity):
     """ oms.MassTraceDetection() """
     def __init__(self, **kwargs):
@@ -79,6 +85,108 @@ class FfmEntity(BaseEntity):
         super(FfmEntity, self).__init__(oms.FeatureFindingMetabo(),
                                                 **kwargs)
 
+class MAEntity(BaseEntity):
+    """oms.MapAlignmentAlgorithmPoseClustering"""
+    def __init__(self, **kwargs):
+        super(MAEntity, self).__init__(oms.MapAlignmentAlgorithmPoseClustering(),
+                                                **kwargs)
+
+class PeakPicking(object):
+
+    def __init__(self,
+                src = ".+\.mzML$",               #"/path/to/src/.+\.mzML"
+                dst = None,                     #/path/to/dst
+                suffix_dst_files = "_centroided",          #for example : "_feature"
+                ext_dst_files = "mzML",#the string may begin with a dot
+                centroid_out_map = None,
+                input_map = None,
+                **kwargs
+                ):
+
+        if not src:
+            raise RuntimeError( "You don`t specify all necessary files" )
+
+        self.src = src
+        self.dst = dst
+        self.suffix_dst_files = suffix_dst_files
+        self.ext_dst_files = ext_dst_files
+        self.kw = kwargs
+        self.input_map = input_map
+        self.centroid_out_map = centroid_out_map
+
+        self.init_entity(**self.kw)
+        self.path_parsing()
+
+    def init_entity(self, **kwargs):
+
+        self.pp = PPEntity(**kwargs)
+
+    def convert_src_to_dst_file_name(self, src):
+        file_name = os.path.splitext( os.path.basename(src) )[0] # get file name only;
+        file_name += self.suffix_dst_files # add suffix;
+        if self.ext_dst_files:
+            #add dot in front of ext if dot is not present:
+            self.ext_dst_files = ("."+self.ext_dst_files) \
+                if self.ext_dst_files[0] != "." else self.ext_dst_files
+        else:
+            self.ext_dst_files = "" #empty ext;
+        dst_file_name = file_name + self.ext_dst_files
+        
+        return dst_file_name    #dst file name without dst dir name;
+
+
+    def path_parsing(self):
+        self.src_full_name_list = []                #list of src full file name;
+        src_dir = os.path.dirname(self.src)         #get dir name from src file name;
+        src_dir = src_dir if src_dir else os.getcwd() #if src dir name is empty get current dir name;
+        self.dst = src_dir if not self.dst else self.dst #if dst dir name is empty get src dir name or dst dir name;
+        pattern = os.path.basename(self.src)        #get file name as pattern;
+        pattern = pattern if pattern != "" else ".*" #if pattern is empty set any char pattern;
+        #print("src_dir=", src_dir)
+        #print("pattern=", pattern)
+        
+        for file_name in os.listdir(src_dir): #for all file name in src dir:
+            full_file_name = os.path.join(src_dir, file_name) #to build full file name;
+            if os.path.isfile( full_file_name ): #only for files, except dir name;
+                #print("file_name=", file_name)
+                match = None    # result re match
+                try:            # try to compile patetrn:
+                    rec = re.compile(pattern)
+                    match = rec.match(file_name)    # apply pattern to file name
+                except re.error as e:
+                    raise RuntimeError("Match pattern error.") # raise exept if pattern error compile;
+                
+                if match: # if result of re match is ok
+                    self.src_full_name_list.append(full_file_name)
+
+
+    def main(self): #the 3 step in one;
+        #after path_parsing method we have self.src_full_name_list
+        for f in self.src_full_name_list:
+
+            # to prepare(init) empty list and entity;
+            self.init_entity(**self.kw)
+
+            print("source file=", f)
+            
+            self.input_map = oms.MSExperiment() # the 1st step: load map;
+
+            oms.MzMLFile().load(f, self.input_map)
+
+            self.centroid_out_map = oms.MSExperiment()
+
+            # the 2nd step: apply_ffm;
+            self.pp.entity.pickExperiment(self.input_map, self.centroid_out_map)
+            
+            self.centroid_out_map.updateRanges()
+
+            # the 3d step: is store result into file;
+            dst_full_file_name = os.path.join(self.dst,\
+                self.convert_src_to_dst_file_name(f) )
+            #print("dst=",dst_full_file_name)
+            oms.MzMLFile().store(dst_full_file_name, self.centroid_out_map)
+
+
 class FeatureFindingMetabo(object):
     
     def __init__(self,
@@ -86,6 +194,8 @@ class FeatureFindingMetabo(object):
                 dst = None,                     #/path/to/dst
                 suffix_dst_files = "_feature",          #for example : "_feature"
                 ext_dst_files = "featureXML",#the string may begin with a dot
+                input_map = None,
+                fm = None,
                 **kwargs
                 ):
         
@@ -98,10 +208,12 @@ class FeatureFindingMetabo(object):
         self.suffix_dst_files = suffix_dst_files
         self.ext_dst_files = ext_dst_files
         self.kw = kwargs
+        self.input_map = input_map
+        self.fm = fm
+        
         self.init_entity(**self.kw)
         self.path_parsing()
 
-    
     def init_entity(self, **kwargs):
         self.mtd = MtdEntity(**kwargs)
         self.epd = EpdEntity(**kwargs)
@@ -148,7 +260,8 @@ class FeatureFindingMetabo(object):
                 
                 if match: # if result of re match is ok
                     self.src_full_name_list.append(full_file_name) #take file name anf put into result list
-                    #print("full_file_name=", full_file_name) #debug only;
+                    #print("full_file_name=", full_file_name) 
+
 
     def main(self): #the 3 step in one;
         #after path_parsing method we have self.src_full_name_list
@@ -157,10 +270,10 @@ class FeatureFindingMetabo(object):
             # to prepare(init) empty list and entity;
             self.init_entity(**self.kw)
 
-            print("source file=", f)
-            self.input_map = None
+            #print("source file=", f)
+            
             self.input_map = oms.PeakMap() # the 1st step: load map;
-            self.fm = None
+            
             self.fm = oms.FeatureMap()
             oms.MzMLFile().load(f, self.input_map)
             # the 2nd step: apply_ffm;
@@ -177,52 +290,104 @@ class FeatureFindingMetabo(object):
 class MapAlignment(object):
 
     def __init__(self,
-                input_file1 = None,  #
-                input_file2 = None,  #
-                output_file1 = None, #
-                output_file2 = None,  #
+                src = ".+\.featureXML$",               #"/path/to/src/.+\.mzML"
+                dst = None,                     #/path/to/dst
+                suffix_dst_files = "_aligned",          
+                ext_dst_files = "featureXML",
+                reference_file = None,
+                reference_map = None,
+                toAlign_map = None,
+                **kwargs
                 ):
         
-        if not input_file1 or not input_file2 or \
-            not output_file1 or not output_file2:
-                raise RuntimeError( "You don`t specify all necessary files" )
+        if not (src and reference_file):
+           raise RuntimeError( "You don`t specify all necessary files" )
         
-        self.input_file1 = input_file1  
-        self.input_file2 = input_file2
-        self.output_file1 = output_file1
-        self.output_file2 = output_file2
-
-        self.reference = reference
-        self.toAlign = toAlign
-        self.algorithm = algorithm
-
-
-    def load_maps(self):
-        self.reference = oms.FeatureMap()
-        self.toAlign = oms.FeatureMap()
-        oms.FeatureXMLFile().load(self.input_file1, self.reference)
-        oms.FeatureXMLFile().load(self.input_file2, self.toAlign)
+        self.src = src
+        self.dst = dst
+        self.suffix_dst_files = suffix_dst_files
+        self.ext_dst_files = ext_dst_files
+        self.kw = kwargs
         
-   
-    def set_algorithm(self): 
-        self.algorithm = BaseEntity(oms.MapAlignmentAlgorithmPoseClustering())
+        self.init_entity(**self.kw)
+        self.path_parsing()
 
-    def apply_align(self, **kwargs):
-        self.load_maps()
-        #self.set_algorithm() # recreate self.algorithm - may be overload;
-        #self.set_parameters(**kwargs) #set default and set custom param;
+        self.reference_file = reference_file
+        self.reference_map = reference_map
+        self.toAlign_map = toAlign_map
+
+
+    def init_entity(self, **kwargs):
+
+        self.ma = MAEntity(**kwargs)
+
+    def convert_src_to_dst_file_name(self, src):
+    
+        file_name = os.path.splitext( os.path.basename(src) )[0] # get file name only;
+        file_name += self.suffix_dst_files # add suffix;
+        if self.ext_dst_files:
+            #add dot in front of ext if dot is not present:
+            self.ext_dst_files = ("."+self.ext_dst_files) \
+                if self.ext_dst_files[0] != "." else self.ext_dst_files
+        else:
+            self.ext_dst_files = "" #empty ext;
+        dst_file_name = file_name + self.ext_dst_files
         
-        self.algorithm.set_parameters(**kwargs)
-        self.algorithm.entity.setReference(self.reference)
-        #create object for the computed transformation
-        transformation = oms.TransformationDescription()  
-        #align
-        self.algorithm.entity.align(self.toAlign, transformation)
+        return dst_file_name    #dst file name without dst dir name;
 
-    def store_to_file(self):
-        #store results
-        oms.FeatureXMLFile().store(self.aligned_file1, self.reference)
-        oms.FeatureXMLFile().store(self.aligned_file2, self.toAlign)
+
+    def path_parsing(self):
+    
+        self.src_full_name_list = []                #list of src full file name;
+        src_dir = os.path.dirname(self.src)         #get dir name from src file name;
+        src_dir = src_dir if src_dir else os.getcwd() #if src dir name is empty get current dir name;
+        self.dst = src_dir if not self.dst else self.dst #if dst dir name is empty get src dir name or dst dir name;
+        pattern = os.path.basename(self.src)        #get file name as pattern;
+        pattern = pattern if pattern != "" else ".*" #if pattern is empty set any char pattern;
+        #print("src_dir=", src_dir)
+        #print("pattern=", pattern)
+        
+        for file_name in os.listdir(src_dir): #for all file name in src dir:
+            full_file_name = os.path.join(src_dir, file_name) #to build full file name;
+            if os.path.isfile( full_file_name ): #only for files, except dir name;
+                #print("file_name=", file_name)
+                match = None    # result re match
+                try:            # try to compile patetrn:
+                    rec = re.compile(pattern)
+                    match = rec.match(file_name)    # apply pattern to file name
+                except re.error as e:
+                    raise RuntimeError("Match pattern error.") # raise exept if pattern error compile;
+                
+                if match: # if result of re match is ok
+                    self.src_full_name_list.append(full_file_name) #take file name anf put into result list
+                    #print("full_file_name=", full_file_name) #debug only;
+    
+
+    def main(self): #the 3 step in one;
+        #after path_parsing method we have self.src_full_name_list
+        
+        for f in self.src_full_name_list:
+
+            self.reference_map = oms.FeatureMap()
+            self.toAlign_map = oms.FeatureMap()
+            
+            oms.FeatureXMLFile().load(self.reference_file, self.reference_map)
+            oms.FeatureXMLFile().load(f, self.toAlign_map)
+            
+            #Set reference_map file
+            self.ma.entity.setReference(self.reference_map)
+            
+            #3rd step create object for the computed transformation
+            transformation = oms.TransformationDescription()
+
+            # the 4rd step:
+            self.ma.entity.align(self.toAlign_map, transformation)
+            # the 5th step: is store result into file;
+            dst_full_file_name = os.path.join(self.dst,\
+                self.convert_src_to_dst_file_name(f) )
+            
+            #print("dst=",dst_full_file_name)
+            oms.FeatureXMLFile().store(dst_full_file_name, self.toAlign_map)
 
 """
 Since I think the good way to overload all parameters of every process,
@@ -282,20 +447,14 @@ class Convert2mgf():
     
 if __name__ == "__main__":
 
-
-    param = {"noise_threshold_int":10.0,
-            "chrom_peak_snr":3.0,
-            "chrom_fwhm":5.0,
-            "mz_scoring_13C":"true",
-            "report_convex_hulls":"true",
-            "remove_single_traces":"true"
-            }
- 
-    ffm = FeatureFindingMetabo(
-                src = "/home/igor/Documents/Scripts/Data/Picked_data/.+\.mzML$",
-                dst = "/home/igor/Documents/Scripts/Data/Picked_data/",
-                suffix_dst_files = "_feature",          #for example : "_feature"
-                ext_dst_files = "featureXML",           #the string may begin with a dot
-                **param)
     
-    ffm.main()
+    param = {"signal_to_noise": 1.0 }
+    
+    a = PeakPicking(
+                src = "/home/igor/Documents/Scripts/Data/Raw_data_STARD10/.+\.mzML$",
+                dst = "/home/igor/Documents/Scripts/Data/Picked_STARD10/",
+                #reference_file = "/home/igor/Documents/Scripts/Data/feature_STARD10/A09_pos_picked_feature.featureXML",
+                suffix_dst_files = "_picked",          #for example : "_feature"
+                ext_dst_files = "mzML",           #the string may begin with a dot
+                **param)
+    a.main()
